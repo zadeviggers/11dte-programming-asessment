8784b01fb02e00b302fa164431519e6eff43de56
Zade Viggers <zadethelad@gmail.com>
2021-04-13 13:48:59 +1200

    docstrings and spelling

--- memory-game.py
+++ memory-game.py
@@ -6,9 +6,6 @@
 
 # Memory game!
 
-import random
-import time
-
 # Board
 #     1   2   3   4
 #   ╔═══╦═══╦═══╦═══╗
@@ -21,6 +18,10 @@ import time
 # 4 ║ # ║ # ║ # ║ A ║
 #   ╚═══╩═══╩═══╩═══╝
 
+import random
+import time
+
+
 # Constants
 #EMOJIS = ["😳","😂","😎","😍","😛","😭","🥵","🥶","🤡","🥳","🤢","👺","👻","☠","👽","👾","🤖","💩","🙉","🙀","🦄","🐲","🐳","🦆","👀","👌","👏","👋","🎃","🎉","💎","🔊","🔒","⌛","🍫","🍕","🍖","🚗","🚂"]
 CHARACTERS = ["A","B","C","D","E","F","G","H","I","J", "K","L","M","N","O","P","P","Q","R", "S", "T","U","V","W", "X", "Y","Z"]
@@ -30,7 +31,6 @@ MAXIMUM_BOARD_SIZE = 20 # For when you're really, REALLY bored
 EXIT_COMMANDS = ["e", "exit"]
 SECONDS_TO_SHOW_BOARD_FOR_AT_START = 3
 
-
 # Variables
 board = [] # Made up of rows represented by lists that hold cells represented by lists that look like: ["A", True] or ["C", False]
 board_size = DEFAULT_BOARD_SIZE
@@ -39,22 +39,38 @@ score = 0
 
 # Functions
 
-# Print an error message
 def print_error(what_to_print, exclamation_mark=True):
+    """Prints a error message.
+
+    Args:
+        what_to_print (str): Error message to print.
+        exclamation_mark (bool, optional): Weather or not to add an exclamation_mark at the end. Defaults to True.
+    """
+    
     if exclamation_mark:
         print("❌  {}!".format(what_to_print))
     else:
         print("❌  {}".format(what_to_print))
 
-# Print a warning message
 def print_warning(what_to_print, exclamation_mark=True):
+    """Prints a warning message.
+
+    Args:
+        what_to_print (str): Warning message to print.
+        exclamation_mark (bool, optional): Weather or not to add an exclamation_mark at the end. Defaults to True.
+    """
     if exclamation_mark:
         print("⚠  {}!".format(what_to_print))
     else:
         print("⚠  {}".format(what_to_print))
 
-# Print a success message
 def print_success(what_to_print, exclamation_mark=True):
+    """Prints a success message.
+
+    Args:
+        what_to_print (str): Success message to print.
+        exclamation_mark (bool, optional): Weather or not to add an exclamation_mark at the end. Defaults to True.
+    """
     if exclamation_mark:
         print("✔  {}!".format(what_to_print))
     else:
@@ -62,7 +78,11 @@ def print_success(what_to_print, exclamation_mark=True):
 
 # Exit the program
 def exit_program(exit_command=None):
-    #print("You scored {}!".format(score))
+    """Exit the program.
+
+    Args:
+        exit_command (str, optional): The command that triggered the exit. Defaults to None.
+    """
 
     if exit_command is not None:
         print_success("Exit command detected: {}".format(exit_command), False)
@@ -70,24 +90,36 @@ def exit_program(exit_command=None):
     print_success("Exiting program")
     exit()
 
-# Increase the score
-def increase_score(amount):
-    score += amount
-    print("Score +{}. Your score is now {}.".format(amount, score))
-
-# Decrease the score
-def decrease_score(amount):
-    score -= amount
-    print("Score -{}. Your score is now {}.".format(amount, score))
+def clear_console():
+    """'clear' the console by dumping a bunch of newlines into it
+    """
+    AMOUNT_TO_DUMP = 420
+    str = "Shoo! Scroll back down!"
+    # Loop a bunch of times and add newlines to a tring
+    for i in range(AMOUNT_TO_DUMP):
+        str+="\n"
+    # Print all the newlines with a single print statement because calling print a bunch of times is slow
+    print(str)
 
 # Get a valid integer input from the user
-def get_integer_input(prompt, minimun=1, maximun=10, default=None):
+def get_integer_input(prompt, minimum=1, maximum=10, default=None):
+    """Gets a valid integer from the user, allowing for maximum and minimum values.
+
+    Args:
+        prompt (str): The message to show to the user when asking for input
+        minimum (int, optional): The minimum value to be accepted. Defaults to 1.
+        maximum (int, optional): The maximum value to be accepted. Defaults to 10.
+        default (str, optional): The value to return if the user inputs nothing. Defaults to None.
+
+    Returns:
+        int: The value provided by the user, or the default value if it was provided and the user inputted nothing
+    """
     value = 0
 
     valid_input = False
     # Loop until a valid input is provided or the user chooses to exit the program
     while not valid_input:
-        print("{} (between {} & {})?".format(prompt, minimun, maximun))
+        print("{} (between {} & {})?".format(prompt, minimum, maximum))
         inputted_value = input("$ ").strip().lower() # Get input from user
 
         # If a default was set and the user inputted nothing, use the default
@@ -107,10 +139,12 @@ def get_integer_input(prompt, minimun=1, maximun=10, default=None):
             # Try and cast the input to an integer
             try:
                 inputted_value = int(inputted_value)
-                if inputted_value > maximun:
-                    print_error("Please enter a integer less than {}".format(maximun+1))
-                elif inputted_value < minimun:
-                    print_error("Please enter a integer greater than {}".format(minimun-1))
+                # Maximum
+                if inputted_value > maximum:
+                    print_error("Please enter a integer less than {}".format(maximum+1))
+                # Minimum
+                elif inputted_value < minimum:
+                    print_error("Please enter a integer greater than {}".format(minimum-1))
                 else:
                     value = inputted_value
                     valid_input = True
@@ -119,18 +153,12 @@ def get_integer_input(prompt, minimun=1, maximun=10, default=None):
 
     return value
 
-# 'clear' the console by dumping a bunch of newlines into it
-def clear_console():
-    AMOUNT_TO_DUMP = 420
-    str = "Shoo! Scroll back down!"
-    # Loop a bunch of times and add newlines to a tring
-    for i in range(AMOUNT_TO_DUMP):
-        str+="\n"
-    # Print all the newlines with a single print statement because calling print a bunch of times is slow
-    print(str)
-
-# Print the board
 def print_board(show_all=False):
+    """Print the board
+
+    Args:
+        show_all (bool, optional): Wether or not to show all the caracters. Defaults to False.
+    """
 
     # Print the column numbers (the numbers along the top)
     column_numbers = "   "
@@ -159,6 +187,8 @@ def print_board(show_all=False):
 
 # Main program
 def main():
+    """The main function for the game.
+    """
 
     # Variables
     total_found_cards = 0
@@ -176,14 +206,13 @@ def main():
     #
 
     # Ask the user how large they would like the board to be
-    board_size = get_integer_input("What size board would you like", minimun=MINIMUM_BOARD_SIZE, maximun=MAXIMUM_BOARD_SIZE, default=DEFAULT_BOARD_SIZE)
+    board_size = get_integer_input("What size board would you like", minimum=MINIMUM_BOARD_SIZE, maximum=MAXIMUM_BOARD_SIZE, default=DEFAULT_BOARD_SIZE)
 
     # generate a set of random pairs
     chars_to_choose_from = []
     i = 1
     while i < (board_size*board_size)+1:
-        #print(i)
-        #print(chars_to_choose_from)
+        # For  every second character, just add the previous character to the list. This ensures that there are pairs
         if i % 2 == 0:
             chars_to_choose_from.append(chars_to_choose_from[i-2])
         else:
@@ -205,7 +234,7 @@ def main():
 
     # Show the board for the size of the board minus 1 seconds
     print_board(True)
-    time.sleep(board_size-1)
+    time.sleep(board_size)
     clear_console()
     print_board()
 
@@ -219,8 +248,8 @@ def main():
         # If there are two or more cards currently revealed (that haven't already been detected as matches), hide them
 
         # Ask the user what card they want to reveal
-        col = get_integer_input("What Column (vertical) do you want to reveal a card from", minimun=1, maximun=board_size)-1
-        row = get_integer_input("What Row (horizontal) do you want to reveal a card from", minimun=1, maximun=board_size)-1
+        col = get_integer_input("What Column (vertical) do you want to reveal a card from", minimum=1, maximum=board_size)-1
+        row = get_integer_input("What Row (horizontal) do you want to reveal a card from", minimum=1, maximum=board_size)-1
         
         
         # Make sure that card isn't already locked
@@ -273,7 +302,7 @@ def main():
                 print_board() # Print the board again
 
 
-
+# Only the the functions if this is being run as a standalone program
 if __name__ == "__main__":
     # Exit gracefully on ctrl+C
     try:
